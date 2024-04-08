01a0c78fe640bc5fbeda49ee890a41b03e854946
Ben Tran <46247492+BenOnSocial@users.noreply.github.com>
2024-04-07 17:27:21 -0700

    Implemented timestamp microservice.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,11 +19,29 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+
+app.get("/api/:timestamp", (req, res) => {
+  let unix = 0;
+  let utc = "";
+
+  if (isNaN(req.params.timestamp)) {
+    unix = Date.parse(req.params.timestamp);
+    utc = new Date(req.params.timestamp).toUTCString();
+  } else {
+    unix = Number(req.params.timestamp);
+    utc = new Date(unix).toUTCString();
+  }
+
+  res.json({
+    unix: unix,
+    utc: utc
+  });
+});
 
 
 // Listen on port set in environment variable or default to 3000
